514c633fd445044f72ceacb6399254348d33710b
James Youngman <james@youngman.org>
2021-12-01 10:32:50 +0000

    [day 01] condensed the code a bit.

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,20 +1,20 @@
 use std::io;
 use std::io::prelude::*;
 
+fn count_increases(values: &[u64]) -> usize {
+    values.windows(2)
+	.filter(|w| w[1] > w[0])
+	.count()
+}
+
 fn main() {
     let depths: Vec<u64> = io::BufReader::new(io::stdin()).lines()
     	.map(|s| s.unwrap().parse::<u64>().unwrap())
 	.collect();
-    let increases = depths.windows(2)
-	.filter(|w| w[1] > w[0])
-	.count();
-    println!("Day 01 part 1: {}", increases);
+    println!("Day 01 part 1: {}", count_increases(&depths));
 
     let sums: Vec<u64> = depths.windows(3)
 	.map(|w| w.iter().sum())
 	.collect();
-    let increases = sums.windows(2)
-	.filter(|w| w[1] > w[0])
-	.count();
-    println!("Day 01 part 2: {}", increases);
+    println!("Day 01 part 2: {}", count_increases(&sums));
 }
