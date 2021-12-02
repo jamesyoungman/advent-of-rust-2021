5a29ed88e46408fe956c12cb749376d799b10f86
James Youngman <james@youngman.org>
2021-12-02 09:10:51 +0000

    Tidied up the day 2 code.

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -2,19 +2,16 @@ use std::io;
 use std::io::prelude::*;
 
 fn count_increases(values: &[u64]) -> usize {
-    values.windows(2)
-	.filter(|w| w[1] > w[0])
-	.count()
+    values.windows(2).filter(|w| w[1] > w[0]).count()
 }
 
 fn main() {
-    let depths: Vec<u64> = io::BufReader::new(io::stdin()).lines()
-    	.map(|s| s.unwrap().parse::<u64>().unwrap())
-	.collect();
+    let depths: Vec<u64> = io::BufReader::new(io::stdin())
+        .lines()
+        .map(|s| s.unwrap().parse::<u64>().unwrap())
+        .collect();
     println!("Day 01 part 1: {}", count_increases(&depths));
 
-    let sums: Vec<u64> = depths.windows(3)
-	.map(|w| w.iter().sum())
-	.collect();
+    let sums: Vec<u64> = depths.windows(3).map(|w| w.iter().sum()).collect();
     println!("Day 01 part 2: {}", count_increases(&sums));
 }
--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -4,71 +4,77 @@ use std::io::prelude::*;
 enum Move {
     Down(i64),
     Up(i64),
-    Forward(i64)
+    Forward(i64),
 }
 
 fn parse_move(s: &str) -> Result<Move, String> {
-    let words: Vec<_> = s.split(" ").collect();
+    let words: Vec<_> = s.split(' ').collect();
     if words.len() != 2 {
-	Err(format!("expected 2 words, got {}: {}", words.len(), s))
+        Err(format!("expected 2 words, got {}: {}", words.len(), s))
     } else {
-	match words[1].parse::<i64>() {
-	    Err(e) => Err(format!("expected a number, got {}: {}", words[1], e)),
-	    Ok(n) => {
-		match words[0] {
-		    "down" => Ok(Move::Down(n)),
-		    "up" => Ok(Move::Up(n)),
-		    "forward" => Ok(Move::Forward(n)),
-		    _ => Err(format!("expected up/down/forward, got {}", words[0])),
-		}
-	    }
-	}
+        match words[1].parse::<i64>() {
+            Err(e) => Err(format!("expected a number, got {}: {}", words[1], e)),
+            Ok(n) => match words[0] {
+                "down" => Ok(Move::Down(n)),
+                "up" => Ok(Move::Up(n)),
+                "forward" => Ok(Move::Forward(n)),
+                _ => Err(format!("expected up/down/forward, got {}", words[0])),
+            },
+        }
     }
 }
 
 fn part1(moves: &[Move]) {
     fn move_sub(pos: (i64, i64), m: &Move) -> (i64, i64) {
-	let (h, v) = pos;
-	match m {
-	    Move::Down(n) => (h, v + n),
-	    Move::Up(n) => (h, v - n),
-	    Move::Forward(n) => (h + n, v),
-	}
+        let (h, v) = pos;
+        match m {
+            Move::Down(x) => (h, v + x),
+            Move::Up(x) => (h, v - x),
+            Move::Forward(x) => (h + x, v),
+        }
     }
 
     println!("Day 02 part 1: {}", {
-	let (h, v) = moves.iter().fold((0, 0), move_sub);
-	h * v
+        let (h, v) = moves.iter().fold((0, 0), move_sub);
+        h * v
     });
 }
-
 
 fn part2(moves: &[Move]) {
     struct Pos {
-	aim: i64,
-	h: i64,
-	v: i64,
+        aim: i64,
+        h: i64,
+        v: i64,
     }
     fn move_sub(pos: Pos, m: &Move) -> Pos {
-	match m {
-	    Move::Down(x) => Pos{ aim: pos.aim + x, h: pos.h, v: pos.v },
-	    Move::Up(x) => Pos{ aim: pos.aim - x, h: pos.h, v: pos.v },
-	    Move::Forward(x) => Pos { aim: pos.aim, h: pos.h + x, v: pos.v + pos.aim * x },
-	}
+        match m {
+            Move::Down(x) => Pos {
+                aim: pos.aim + x,
+		..pos
+            },
+            Move::Up(x) => Pos {
+                aim: pos.aim - x,
+		..pos
+            },
+            Move::Forward(x) => Pos {
+                h: pos.h + x,
+                v: pos.v + pos.aim * x,
+		..pos
+            },
+        }
     }
 
     println!("Day 02 part 2: {}", {
-	let start = Pos { aim: 0, h: 0, v: 0 };
-	let end = moves.iter().fold(start, move_sub);
-	end.h * end.v
+        let end = moves.iter().fold(Pos { aim: 0, h: 0, v: 0 }, move_sub);
+        end.h * end.v
     });
 }
 
-
 fn main() {
-    let moves: Vec<Move> = io::BufReader::new(io::stdin()).lines()
-    	.map(|s| parse_move(&s.unwrap()).expect("valid input"))
-	.collect();
+    let moves: Vec<Move> = io::BufReader::new(io::stdin())
+        .lines()
+        .map(|s| parse_move(&s.unwrap()).expect("valid input"))
+        .collect();
     part1(&moves);
     part2(&moves);
 }
