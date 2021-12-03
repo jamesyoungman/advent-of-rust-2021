438eb3141e850eb21900e76fe3ee635629e28df0
James Youngman <james@youngman.org>
2021-12-03 11:12:40 +0000

    Solved day03 part 2.

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::cmp::Ordering;
 use std::io;
 use std::io::prelude::*;
 
@@ -6,59 +6,102 @@ fn parse_binary(s: &str) -> i32 {
     i32::from_str_radix(s, 2).expect("valid input")
 }
 
-fn count_bit_occupants(
-    bitwidth: i32,
-    mut h: BTreeMap<i32, usize>,
-    n: &i32
-) -> BTreeMap<i32, usize> {
-    for bitpos in 0..bitwidth {
-	let mask: i32 = 1 << bitpos;
-	let value = n & mask == mask;
-	if value {
-	    *h.entry(bitpos as i32).or_insert(0) += 1
-	}
-    }
-    h
+fn mask(bitwidth: i32, bitpos: i32) -> i32 {
+    1 << (bitwidth - bitpos - 1)
 }
 
-fn part1(
-    bitwidth: i32,
-    readings: &Vec<i32>
-) {
-    let counts: BTreeMap<i32, usize> = readings.iter()
-	.fold(BTreeMap::new(),
-	      |acc, n| count_bit_occupants(bitwidth, acc, n));
+fn most_popular(bitwidth: i32, bitpos: i32, readings: &[i32]) -> Option<bool> {
+    let mask: i32 = mask(bitwidth, bitpos);
+    let mut count1: usize = 0;
+    for n in readings {
+        if (n & mask) == mask {
+            count1 += 1
+        }
+    }
+    let count0 = readings.len() - count1;
+    let result: Option<bool> = match count1.cmp(&count0) {
+        Ordering::Greater => Some(true),
+        Ordering::Less => Some(false),
+        Ordering::Equal => None,
+    };
+    //println!(
+    //	"most_popular: bit position {}: count of 1s: {}, count of 0s: {}, most popular: {:?}",
+    //	bitpos,
+    //	count1,
+    //	count0,
+    //	result
+    //);
+    result
+}
+
+fn part1(bitwidth: i32, readings: &[i32]) {
     let mut gamma = 0;
     for bitpos in 0..bitwidth {
-	let count1 = counts[&bitpos];
-	//println!("part1: readings.len()={}, counts[{}]={}", readings.len(), bitpos, count1);
-	let count0 = readings.len() - count1;
-	if count1 > count0 {
-	    println!("bit pos {}: most common: 1", bitpos);
-	    gamma |= 1 << bitpos;
-	} else {
-	    println!("bit pos {}: most common: 0", bitpos);
-	}
+        if most_popular(bitwidth, bitpos, readings) == Some(true) {
+            gamma |= mask(bitwidth, bitpos);
+        }
     }
-    println!("Part 1: gamma={:b}={}", gamma, gamma);
-    let mask: i32 = (1 << bitwidth) - 1;
-    let epsilon = (!gamma) & mask;
-    println!("Part 1: epsilon={:b}={}", epsilon, epsilon);
-    println!("Part 1: gamma*epsilon={}", gamma*epsilon);
-
+    //println!("Part 1: gamma={:b}={}", gamma, gamma);
+    let all_bits: i32 = (1 << bitwidth) - 1;
+    let epsilon = (!gamma) & all_bits;
+    println!("Part 1: {}*{} = {}", gamma, epsilon, gamma * epsilon);
 }
 
+fn filter(bitwidth: i32, readings: &[i32], retain_most_popular: bool) -> i32 {
+    let mut remaining = Vec::from(readings);
+
+    for bitpos in 0..bitwidth {
+        let most_popular = most_popular(bitwidth, bitpos, &remaining);
+        let expected = if retain_most_popular {
+            match most_popular {
+                Some(b) => b,
+                None => retain_most_popular,
+            }
+        } else {
+            match most_popular {
+                Some(b) => !b,
+                None => retain_most_popular,
+            }
+        };
+        //println!("filter: bit pos {}: retaining readings with value {}",
+        //	 bitpos, if expected { 1 } else { 0 });
+        let want = |x: &i32| {
+            let mask = mask(bitwidth, bitpos);
+            let got = x & mask == mask;
+            expected == got
+        };
+        //for n in &remaining {
+        //    println!("before filtering: {:05b}", n);
+        //}
+        remaining.retain(want);
+        //for n in &remaining {
+        //    println!("after filtering: retained: {:05b}", n);
+        //}
+        if remaining.len() == 1 {
+            break;
+        }
+    }
+    remaining[0]
+}
+
+fn part2(bitwidth: i32, readings: &[i32]) {
+    let oxygen_generator_reading = filter(bitwidth, readings, true);
+    let co2_scrubber_reading = filter(bitwidth, readings, false);
+    println!(
+        "Day 3 part 2: {}*{} = {}",
+        oxygen_generator_reading,
+        co2_scrubber_reading,
+        oxygen_generator_reading * co2_scrubber_reading,
+    );
+}
 
 fn main() {
     let lines: Vec<String> = io::BufReader::new(io::stdin())
         .lines()
-	.map(|line| line.unwrap())
-	.collect();
-    println!("[{}]", lines[0]);
-    let bitwidth = lines[0].len() as i32;
-    let readings: Vec<i32> =
-	lines.iter()
-        .map(|line| parse_binary(&line))
+        .map(|line| line.unwrap())
         .collect();
+    let bitwidth = lines[0].len() as i32;
+    let readings: Vec<i32> = lines.iter().map(|line| parse_binary(line)).collect();
     part1(bitwidth, &readings);
+    part2(bitwidth, &readings);
 }
