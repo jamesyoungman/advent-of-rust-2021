65905b0bcdab03f346b3de41275cead6c6d25917
James Youngman <james@youngman.org>
2021-12-14 23:39:29 +0000

    Create a blank template for day 15.

---
+++ src/bin/day15.rs
@@ -0,0 +1,19 @@
+use std::io;
+use std::io::prelude::*;
+
+
+fn part1() {
+    println!("Day 15 part 1: ");
+}
+
+fn part2() {
+    println!("Day 15 part 2: ");
+}
+
+
+fn main() {
+    let _lines: Vec<String> = io::BufReader::new(io::stdin())
+        .lines()
+        .map(|thing| thing.unwrap())
+        .collect();
+}
