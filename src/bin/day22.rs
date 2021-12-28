d32a934f2789759a7e932e56f4d37e5124bf5586
James Youngman <james@youngman.org>
2021-12-28 08:19:27 +0000

    Updated day 22 code, still not complete

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -1,7 +1,6 @@
 use std::fmt::{self, Display, Formatter};
 use std::io;
 use std::io::prelude::*;
-use std::iter::Extend;
 
 mod base {
     use core::ops::RangeInclusive;
@@ -26,7 +25,7 @@ mod base {
         }
     }
 
-    #[derive(Debug, PartialEq, Eq)]
+    #[derive(Debug, PartialEq, Eq, Clone)]
     pub struct Range3D {
         pub x: RangeInclusive<i32>,
         pub y: RangeInclusive<i32>,
@@ -43,8 +42,113 @@ mod base {
     }
 
     impl Range3D {
+        pub fn intersect(&self, other: &Range3D) -> Range3D {
+            Range3D {
+                x: max(*self.x.start(), *other.x.start())..=min(*self.x.end(), *other.x.end()),
+                y: max(*self.y.start(), *other.y.start())..=min(*self.y.end(), *other.y.end()),
+                z: max(*self.z.start(), *other.z.start())..=min(*self.z.end(), *other.z.end()),
+            }
+        }
+    }
+
+    #[derive(Debug)]
+    pub enum PointLineRelation {
+        Before,
+        Within,
+        Beyond,
+    }
+
+    fn relation_of_point_to_range(val: i32, range: &RangeInclusive<i32>) -> PointLineRelation {
+        if range.start() > &val {
+            PointLineRelation::Before
+        } else if range.end() < &val {
+            PointLineRelation::Beyond
+        } else {
+            PointLineRelation::Within
+        }
+    }
+
+    impl Range3D {
         fn is_empty(&self) -> bool {
             self.x.is_empty() || self.y.is_empty() || self.z.is_empty()
+        }
+
+        pub fn count_points(&self) -> u64 {
+            if self.is_empty() {
+                0
+            } else {
+                let x_size: u64 = (self.x.end() - self.x.start() + 1)
+                    .try_into()
+                    .expect("x range should have a positive number of entries");
+                let y_size: u64 = (self.y.end() - self.y.start() + 1)
+                    .try_into()
+                    .expect("y range should have a positive number of entries");
+                let z_size: u64 = (self.z.end() - self.z.start() + 1)
+                    .try_into()
+                    .expect("z range should have a positive number of entries");
+                x_size * y_size * z_size
+            }
+        }
+
+        /// Split a Range3D into (a, b) by x coordinate (i.e. cut it with a plane
+        /// parallel to the y,z axes).
+        /// a is the points having x <= xboundary
+        /// b is the points having x > xboundary
+        pub fn split_x(self, xboundary: i32) -> (Option<Range3D>, Option<Range3D>) {
+            match relation_of_point_to_range(xboundary, &self.x) {
+                PointLineRelation::Before => (None, Some(self)),
+                PointLineRelation::Beyond => (Some(self), None),
+                PointLineRelation::Within => (
+                    Some(Range3D {
+                        x: *self.x.start()..=xboundary,
+                        y: self.y.clone(),
+                        z: self.z.clone(),
+                    }),
+                    Some(Range3D {
+                        x: (xboundary + 1)..=*self.x.end(),
+                        y: self.y,
+                        z: self.z,
+                    }),
+                ),
+            }
+        }
+
+        pub fn split_y(self, yboundary: i32) -> (Option<Range3D>, Option<Range3D>) {
+            match relation_of_point_to_range(yboundary, &self.y) {
+                PointLineRelation::Before => (None, Some(self)),
+                PointLineRelation::Beyond => (Some(self), None),
+                PointLineRelation::Within => (
+                    Some(Range3D {
+                        x: self.x.clone(),
+                        y: *self.y.start()..=yboundary,
+                        z: self.z.clone(),
+                    }),
+                    Some(Range3D {
+                        x: self.x,
+                        y: (yboundary + 1)..=*self.y.end(),
+                        z: self.z,
+                    }),
+                ),
+            }
+        }
+
+        pub fn split_z(self, zboundary: i32) -> (Option<Range3D>, Option<Range3D>) {
+            match relation_of_point_to_range(zboundary, &self.z) {
+                PointLineRelation::Before => (None, Some(self)),
+                PointLineRelation::Beyond => (Some(self), None),
+                PointLineRelation::Within => (
+                    Some(Range3D {
+                        x: self.x.clone(),
+                        y: self.y.clone(),
+                        z: *self.z.start()..=zboundary,
+                    }),
+                    Some(Range3D {
+                        x: self.x,
+                        y: self.y,
+                        z: (zboundary + 1)..=*self.z.end(),
+                    }),
+                ),
+            }
         }
 
         pub fn crop(&self) -> Option<Range3D> {
@@ -61,6 +165,120 @@ mod base {
         pub fn contains(&self, p: &Point) -> bool {
             self.x.contains(&p.0) && self.y.contains(&p.1) && self.z.contains(&p.2)
         }
+    }
+
+    #[test]
+    fn test_range3d_split_x() {
+        let r = Range3D {
+            x: 5..=10,
+            y: 20..=30,
+            z: 50..=90,
+        };
+        assert_eq!(r.clone().split_x(1000000), (Some(r), None));
+
+        let r = Range3D {
+            x: 5..=10,
+            y: 20..=30,
+            z: 50..=90,
+        };
+        assert_eq!(r.clone().split_x(-1000000), (None, Some(r)));
+
+        let r = Range3D {
+            x: 5..=10,
+            y: 20..=30,
+            z: 50..=90,
+        };
+        assert_eq!(
+            r.clone().split_x(8),
+            (
+                Some(Range3D {
+                    x: 5..=8,
+                    y: 20..=30,
+                    z: 50..=90,
+                }),
+                Some(Range3D {
+                    x: 9..=10,
+                    y: 20..=30,
+                    z: 50..=90,
+                })
+            )
+        );
+    }
+
+    #[test]
+    fn test_range3d_split_y() {
+        let r = Range3D {
+            x: 20..=30,
+            y: 5..=10,
+            z: 50..=90,
+        };
+        assert_eq!(r.clone().split_y(1000000), (Some(r), None));
+
+        let r = Range3D {
+            x: 20..=30,
+            y: 5..=10,
+            z: 50..=90,
+        };
+        assert_eq!(r.clone().split_y(-1000000), (None, Some(r)));
+
+        let r = Range3D {
+            x: 20..=30,
+            y: 5..=10,
+            z: 50..=90,
+        };
+        assert_eq!(
+            r.clone().split_y(8),
+            (
+                Some(Range3D {
+                    x: 20..=30,
+                    y: 5..=8,
+                    z: 50..=90,
+                }),
+                Some(Range3D {
+                    x: 20..=30,
+                    y: 9..=10,
+                    z: 50..=90,
+                })
+            )
+        );
+    }
+
+    #[test]
+    fn test_range3d_split_z() {
+        let r = Range3D {
+            x: 20..=30,
+            y: 50..=90,
+            z: 5..=10,
+        };
+        assert_eq!(r.clone().split_z(1000000), (Some(r), None));
+
+        let r = Range3D {
+            x: 20..=30,
+            y: 50..=90,
+            z: 5..=10,
+        };
+        assert_eq!(r.clone().split_z(-1000000), (None, Some(r)));
+
+        let r = Range3D {
+            x: 20..=30,
+            y: 50..=90,
+            z: 5..=10,
+        };
+        assert_eq!(
+            r.clone().split_z(8),
+            (
+                Some(Range3D {
+                    x: 20..=30,
+                    y: 50..=90,
+                    z: 5..=8,
+                }),
+                Some(Range3D {
+                    x: 20..=30,
+                    y: 50..=90,
+                    z: 9..=10,
+                })
+            )
+        );
     }
 
     #[test]
@@ -223,6 +441,11 @@ mod base {
             }
         }
     }
+
+    pub trait CubeFlipper {
+        fn count_cubes_on(&self) -> u64;
+        fn obey(&mut self, instruction: &Instruction);
+    }
 }
 
 mod part1 {
@@ -230,7 +453,7 @@ mod part1 {
 
     use super::base::*;
 
-    struct Reactor {
+    pub struct Reactor {
         on: HashSet<Point>,
     }
 
@@ -247,38 +470,84 @@ mod part1 {
     }
 
     impl Reactor {
-        fn new() -> Reactor {
+        pub fn new() -> Reactor {
             Reactor {
                 on: HashSet::with_capacity(101 * 101 * 101),
             }
         }
+    }
 
-        fn count_cubes_on(&self) -> usize {
-            self.on.len()
+    impl CubeFlipper for Reactor {
+        fn count_cubes_on(&self) -> u64 {
+            self.on.len() as u64
         }
 
         fn obey(&mut self, instruction: &Instruction) {
             match instruction {
                 Instruction::On(range) => {
-                    self.on.extend(all(instruction.affects()));
+                    self.on.extend(all(range));
                 }
                 Instruction::Off(range) => self.on.retain(|p| !range.contains(p)),
             }
         }
     }
+}
 
-    pub fn run(instructions: &[Instruction]) {
-        let cropped: Vec<Instruction> =
-            instructions.iter().filter_map(|inst| inst.crop()).collect();
-        let mut reactor = Reactor::new();
-        for instruction in &cropped {
-            reactor.obey(instruction);
+mod part2 {
+    use super::base::*;
+
+    fn run(instructions: &[Instruction]) -> u64 {
+        let mut lit: Vec<Range3D> = Vec::new();
+        let mut overcount_lit: u64 = 0;
+        let mut overcount_off: u64 = 0;
+
+        for instruction in instructions {
+            match instruction {
+                Instruction::On(range) => {
+                    for r in &lit {
+                        let intersection = r.intersect(range);
+                        overcount_lit += intersection.count_points();
+                    }
+                    lit.push(range.clone());
+                }
+                Instruction::Off(range) => {
+                    for r in &lit {
+                        let intersection = r.intersect(range);
+                        overcount_off += intersection.count_points();
+                    }
+                }
+            }
         }
-        println!("Day 22 part 1: {} cubes are on", reactor.count_cubes_on());
+        let tmp = lit.iter().map(|r| r.count_points()).sum::<u64>();
+        dbg!(&tmp);
+        dbg!(&overcount_lit);
+        dbg!(&overcount_off);
+        assert!(tmp > overcount_lit);
+        assert!(tmp > overcount_off);
+        dbg!(dbg!(tmp) - dbg!(overcount_lit))
+    }
+
+    pub fn part2(instructions: &[Instruction]) {
+        let count = run(instructions);
+        // 272176456701857 is too low.
+        println!("Day 22 part 2: cubes lit: {}", count);
     }
 }
 
+use base::CubeFlipper;
 use base::Instruction;
+
+pub fn run_part<T: CubeFlipper>(part: i32, instructions: &[Instruction], reactor: &mut T) {
+    let cropped: Vec<Instruction> = instructions.iter().filter_map(|inst| inst.crop()).collect();
+    for instruction in &cropped {
+        reactor.obey(instruction);
+    }
+    println!(
+        "Day 22 part {}: {} cubes are on",
+        part,
+        reactor.count_cubes_on()
+    );
+}
 
 fn run() -> Result<(), String> {
     let instructions: Vec<Instruction> = io::BufReader::new(io::stdin())
@@ -289,7 +558,10 @@ fn run() -> Result<(), String> {
     for instruction in &instructions {
         println!("{:?}", instruction);
     }
-    part1::run(&instructions);
+    let mut reactor1 = part1::Reactor::new();
+    run_part(1, &instructions, &mut reactor1);
+
+    part2::part2(&instructions);
     Ok(())
 }
 
